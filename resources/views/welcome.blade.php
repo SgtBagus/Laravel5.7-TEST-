96e41a626f91ef2e937405baa3200f9314d98967
SgtBagus <procw57@gmail.com>
2019-02-28 13:15:25 +0700

    Bagus Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-		Aron Coba Update
+		Aron Coba Update, di tambai sesuatu disini XD
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
